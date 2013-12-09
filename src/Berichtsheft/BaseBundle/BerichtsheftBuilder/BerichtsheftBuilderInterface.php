35a765e991f3d8f0be365c8d50333f9bcfc3f0a6
PalliDotCom <pallidotcom@gmail.com>
2013-12-09 14:27:59 +0100

    getWorklogRetriever ins Interface gezogen

--- src/Berichtsheft/BaseBundle/BerichtsheftBuilder/BerichtsheftBuilderInterface.php
+++ src/Berichtsheft/BaseBundle/BerichtsheftBuilder/BerichtsheftBuilderInterface.php
@@ -4,6 +4,7 @@ namespace Berichtsheft\BaseBundle\BerichtsheftBuilder;
 
 use Berichtsheft\BaseBundle\Model\AzubiInterface;
 use Berichtsheft\BaseBundle\Model\Berichtsheft;
+use Berichtsheft\BaseBundle\Worklog\WorklogRetriever;
 
 /**
  * Interface BerichtsheftBuilderInterface
@@ -16,7 +17,12 @@ interface BerichtsheftBuilderInterface
    * @param int $week
    * @param int $year
    * @param int $number
-   * @return mixed
+   * @return Berichtsheft
    */
   function generateBerichtsheft(AzubiInterface $azubi, $week, $year, $number = 1);
+
+  /**
+   * @return WorklogRetriever
+   */
+  function getWorklogRetriever();
 } 
